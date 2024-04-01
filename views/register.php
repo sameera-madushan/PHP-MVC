287595ceb60f0c5f57cb09cc97aef08a4f07ee10
sameeraMadushan <sameera@pramixit.com>
2024-04-01 23:18:27 +0530

    Register Model added

--- views/register.php
+++ views/register.php
@@ -1,12 +1,53 @@
-<h1>Register</h1>
-<form action="" method="post">
-    <div class="form-group">
-        <label>Subject</label>
-        <input type="text">
+<style>
+    body,
+    html {
+      height: 100%;
+      padding-top: 10px;
+      padding-bottom: 10px;
+    }
+</style>
+
+<section>
+  <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+    <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div class="card" style="border-radius: 15px;">
+            <div class="card-body p-5">
+              <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+
+              <form action="" method="post">
+
+                <div class="form-outline mb-4">
+                  <input name="name" type="text" id="form3Example1cg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example1cg">Your Name</label>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input name="email" type="email" id="form3Example3cg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example3cg">Your Email</label>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input name="password" type="text" id="form3Example4cg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example4cg">Password</label>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <input name="passwordConfirm" type="text" id="form3Example4cdg" class="form-control form-control-lg" />
+                  <label class="form-label" for="form3Example4cdg">Repeat your password</label>
+                </div>
+
+                <div class="d-flex justify-content-center">
+                  <button type="submit"
+                    class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
+                </div>
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="form-group">
-        <label>Name</label>
-        <input type="text">
-    </div>
-    <button type="submit">Submit</button>
-</form>
+  </div>
+</section>
